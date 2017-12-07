a6555ca77f21a5b9cf7832bd2c066b7134e402c0
Taikon <davidma100@gmail.com>
2017-12-06 17:35:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/user/androidtest0/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.androidtest0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //R.layout is allows you to identify content in the res folder.
+        //setContentView lets you set those content in the res folder into different locations in your RelativeView/LinearView/etcView.
+
+        private void TestUpload{
+            int i = 1+1;
+        }
+    }
+}
